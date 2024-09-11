70d2bd88f7bff18eb24e33da943103c6ae0a69d3
Rick269599 <corpin.269599@dasmarinas.sti.edu.ph>
2024-09-12 00:04:30 +0800

    Practice Task for printing command.

---
+++ FavoriteSong/src/favoritesong/FavoriteSong.java
@@ -0,0 +1,13 @@
+package favoritesong;
+
+public class FavoriteSong {
+
+    public static void main(String[] args) {
+        System.out.println("Satan sucks, but you're the best."); //Explaining a little about herself and how she's religious.
+        System.out.println("Holy smokes, you passed the test."); //Expressing how good he is to her that he fits into all her standards.
+        System.out.println("When I'm with you, I feel blessed."); //She is showing her appreciation of his presence in her life.
+        System.out.println("My chinchilla."); //Giving him an affectionate nickname and likening him to an adorable animal.
+        
+    }
+    
+}
